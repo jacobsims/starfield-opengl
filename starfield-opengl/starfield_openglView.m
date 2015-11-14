b9621d26372e7a877ea23e00a22e4d0b61bfbbaf
Jacob Sims <jacobsims@users.noreply.github.com>
2015-11-14 10:24:26 -0500

    Initial Commit

---
+++ starfield-opengl/starfield_openglView.m
@@ -0,0 +1,52 @@
+//
+//  starfield_openglView.m
+//  starfield-opengl
+//
+//  Created by Jacob Sims on 2015-11-14.
+//  Copyright © 2015 Jacob Sims. All rights reserved.
+//
+
+#import "starfield_openglView.h"
+
+@implementation starfield_openglView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
