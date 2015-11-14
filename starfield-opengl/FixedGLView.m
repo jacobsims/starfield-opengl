35eb7b52dcf72d6b532abbceb32b6cffbdec6cb5
Jacob Sims <jacobsims@users.noreply.github.com>
2015-11-14 12:28:36 -0500

    Add license

--- starfield-opengl/FixedGLView.m
+++ starfield-opengl/FixedGLView.m
@@ -2,9 +2,6 @@
 //  FixedGLView.m
 //  starfield-opengl
 //
-//  Created by Jacob Sims on 2015-11-14.
-//  Copyright © 2015 Jacob Sims. All rights reserved.
-//
 
 #import "FixedGLView.h"
 
--- starfield-opengl/starfield_openglView.m
+++ starfield-opengl/starfield_openglView.m
@@ -2,9 +2,6 @@
 //  starfield_openglView.m
 //  starfield-opengl
 //
-//  Created by Jacob Sims on 2015-11-14.
-//  Copyright © 2015 Jacob Sims. All rights reserved.
-//
 
 #import "starfield_openglView.h"
 
